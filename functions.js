a8c64c70a4fb26c51bf5d4c791be235821178409
meek2020 <55003545+meek2020@users.noreply.github.com>
2023-04-28 12:49:09 -0700

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,23 @@
+// http://ahrengot.com/tutorials/greensock-javascript-animation
+
+var $cursor = $('.cursor');
+
+function moveCursor(e) {
+  $cursor.addClass('is-moving');
+  
+  TweenLite.to($cursor, 0.23, {
+    left: e.pageX,
+    top: e.pageY,
+    ease: Power4.easOut
+  });
+  
+  clearTimeout(timer);
+
+   var timer = setTimeout(function() {
+       $cursor.removeClass('is-moving');
+   }, 300);
+}
+
+$(window).on('mousemove', moveCursor);
+
+
